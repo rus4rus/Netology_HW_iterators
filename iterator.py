a0d10592f0126c3f9a48d532756d71c983d9f7bd
Ruslan Shakirov <shakirov3000@mail.ru>
2022-09-18 21:22:25 +0500

    init

--- iterator.py
+++ iterator.py
@@ -8,8 +8,8 @@ class FlatIterator:
     def __init__(self, list_: list):
         self.list_ = list_
         self.item = []
-        self.i = 0
-        self.k = 0
+        self.i = 0 # индекс главного списка
+        self.k = 0 # индекс вложенного списка
 
     def __iter__(self):
         return self
@@ -17,7 +17,10 @@ class FlatIterator:
     def __next__(self):
         if self.i == len(self.list_):
             raise StopIteration
-        if self.k == len(self.list_[self.i]):
+        elif self.k == len(self.list_[self.i]):
+            # Если дошли до конца вложенного списка,
+            # то возвращаем 1-й элемент следующего списка (после проверки на StopIterarion)
+            # и далее обрабатываем уже со 2го элемента следующего списка
             self.k = 1
             self.i += 1
             if self.i == len(self.list_):
